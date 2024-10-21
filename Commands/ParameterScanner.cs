4ef0cff81eca1164ccc0a0e9e741d75bbb87af29
Kevin <kevzu72@gmail.com>
2024-10-21 15:07:04 -0300

    Manifiesto y carpetas

---
+++ Commands/ParameterScanner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace Parameters
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    internal class ParameterScanner : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Application app = uiApp.Application;
+            Document doc = uiDoc.Document;
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 
 namespace Parameters
 {
-    public class Main
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Main : IExternalApplication
     {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            string RutaDelEnsamblado = System.Reflection.Assembly.GetExecutingAssembly().Location;
 
+            RibbonPanel panelDetalleArmado = application.CreateRibbonPanel("Parameters", "Parameters");
+
+            PushButton botonParameter = panelDetalleArmado.AddItem(new PushButtonData("btnParameter", "Parameter Scanner", RutaDelEnsamblado, "Parameters.cmdParameterScanner")) as PushButton;
+
+            botonParameter.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Jump;component/Resources/Ico.png"));
+
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
     }
 }
